7849b34abd8e29f1e84f40ee45945bc249d545ed
Phil Yoo <philyoomail@gmail.com>
2018-12-17 23:42:21 -0500

    another typo

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -7,7 +7,7 @@ import './AboutMe.css';
 import aeLogo from './icons/adobe_after_effects.png'
 import aiLogo from './icons/adobe_illustrator.png'
 import css from './icons/css.png'
-import django from './icon/django.png'
+import django from './icons/django.png'
 import expressLogo from './icons/expressjs.png'
 import headShot from './screencaps/phil_headshot.jpg'
 import html from './icon/html.png'
